d1164df85f486505688a1c1c6fa84500df9e50ab
-v <wtrzos@gmail.com>
2018-06-15 16:20:40 +0200

    totolotek

---
+++ src/Lotto/c_zgadywanie1000.java
@@ -0,0 +1,47 @@
+package Lotto;
+
+import java.util.Scanner;
+
+public class c_zgadywanie1000 {
+
+    public static void main(String[] args) {
+        int min=1, max=1000, gues, count=0;
+
+
+        boolean zwyciestwo = false;
+        System.out.println("Wymyśl liczbę od 1 do 1000");
+        while(!zwyciestwo) {
+            Scanner scan = new Scanner(System.in);
+            gues = (max - min) / 2+min;
+            System.out.println("Proba " + ++count + ": Czy twoja liczba to:" + gues + "?");
+            String odp = "";
+
+            while (!"t".equals(odp) && !"w".equals(odp) && !"m".equals(odp)) {
+                System.out.println("Odpowiedz << t >>jeśli zgadłem, << m >> jeśli Twoja liczba jest mniejsza, << w >>, jeśli Twoja liczba jest większa");
+                odp = scan.nextLine();
+            }
+
+            switch (odp) {
+                case "t":
+                    System.out.println("Wygrałem!");
+                    return;
+
+                case "w":
+                    min = gues;
+                    break;
+                case "m":
+                    max = gues;
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
